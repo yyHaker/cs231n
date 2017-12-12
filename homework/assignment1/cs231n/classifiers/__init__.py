56bf3225671838cd36dc82b07c53e5716b6a116c
yy <572176750@qq.com>
2017-12-12 22:20:46 +0800

    Initial commit: create the repository

---
+++ homework/assignment1/cs231n/classifiers/__init__.py
@@ -0,0 +1,2 @@
+from homework.assignment1.cs231n.classifiers.k_nearest_neighbor import *
+from homework.assignment1.cs231n.classifiers.linear_classifier import *
---
+++ readme.md
@@ -0,0 +1,4 @@
+CS231n courses
+---------
+1. course link: http://cs231n.stanford.edu/index.html
+2. python environment: py3.5 + (requirments.txt)
